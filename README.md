471691d3a3cea32416304234c307df854ee67527
Kenny Luu <kluu006@ucr.edu>
2015-04-17 17:05:07 -0700

    fixed some logic errors, update README

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # rshell
+
+Rshell is my version of the BASH terminal. Rshell can run commands using connectors such as `;` and `||` and `&&` or no connectors at all. Anything after a # will be considered a comment, and is disregarded when runnings commands.
+
+The connector `||` means that if the first command is true, then do not run the second command. If the first command is false, then run the second command.
+
+The connector `&&` means that if the first command is true, then run the second command. If the first command is false, then does not run the second command.
+
+The connector `;` will run any command if the command is true.
+
+Running the command `true` will return true. Running the command false will return false.
+
+Having an empty command returns false. There is no error output.
+
+Comments are marked as `#`. Anything after the comment will not be ran.
+
+## How to run rshell
+
+```
+$git clone https://github.com/kluu006/rshell.git
+$cd rshell
+$git checkout hw0
+$make
+$bin/rshell
+
+```
+## Bugs / Limitations
+
+```
+Anything under quotes will not work.
+echo can output standard in; however, cannot write to a file with quotations.
+
+```
+
+## Licensing
+
+Licensing can be found * [here](/LICENSE)
--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,12 @@ using namespace std;
 
 void display_name()
 {
-	/*char* login = getlogin();
+	char* login = getlogin();
 	if(!getlogin()) //displays the username
 		perror("getlogin");
-	*/
-	char log[BUFSIZ];
-	getlogin_r(log, BUFSIZ);
+	//char log[BUFSIZ];
+	//getlogin_r(log, BUFSIZ);
+	//	perror("getlogin_r");
 	char host_name[BUFSIZ];
 	if(gethostname(host_name, BUFSIZ)== -1) //displays the host name
 		perror("gethostname");
@@ -25,11 +25,11 @@ void display_name()
 	if(getcwd(directory_name, BUFSIZ) == NULL) //displays the directory
 		perror("getcwd");*/
 
-	cout << log  << "@" << host_name  << ":" /*<< directory_name*/ << "$ ";
+	cout << login  << "@" << host_name  << ":" /*<< directory_name*/ << "$ ";
 	
 }
 
-bool contain_exit(char** terminate)
+bool contain_exit(char** terminate)	//check if exit
 {
 	string exit_p = *terminate;
 	if(exit_p == "exit")
@@ -47,21 +47,21 @@ void tokenizer(string& conditional, char**& lol, char*& the_token)
 	int count = 0;
 	size_t neg_f = -1;
 	char** new_lol = (char**)malloc(BUFSIZ);
-	bool token_flag = false;
+	bool token_flag = false;			// the parsing
 	while(!token_flag && the_token != NULL)
 	{
 		string b = the_token;
 		if((b.find(or_c) != neg_f) || (b.find(and_c) != neg_f) || (b.find(semi_c) != neg_f))
 		{
-			token_flag = true;
+			token_flag = true;	//if found connector, end parse
 		}
 		if(!token_flag)
 		{
-			new_lol[count] = the_token;
+			new_lol[count] = the_token;	//pushes everything before end of parse into char**
 		}
 		else
 		{
-			new_lol[count] = NULL;
+			new_lol[count] = NULL;		//remember to have NULL at the end
 			conditional = b;
 		}
 		count++;
@@ -69,14 +69,14 @@ void tokenizer(string& conditional, char**& lol, char*& the_token)
 	}
 	if(!token_flag)
 	{
-		new_lol[count] = NULL;
+		new_lol[count] = NULL;		// if no connector still add NULL
 	}
 	lol = new_lol;
-	//return lol;
 }
 
 bool run_execvp(char** arg)
 {
+	int status = 0;
 	if(*arg == NULL)
 		return false;
 	int pid = fork();
@@ -85,29 +85,32 @@ bool run_execvp(char** arg)
 		perror("fork");
 		exit(1);
 	}
-	else if(pid == 0)
+	else if(pid == 0)	//this be child
 	{
-			if(-1 != execvp(arg[0], arg))
-				return true;
-			else
-			{
-				perror("execvp");
-				exit(1);
-			}
+		status = execvp(arg[0], arg);
+		if(status == -1)
+		{
+			perror("execvp");
+			exit(1);		//exit if error
+		}
 			
 	}
 	else if(pid > 0)
 	{
-		if(wait(0) == -1)
-			perror("There was an error waiting"); //somehow an error during waiting
-		//char** argv = arg;
+		int r = waitpid(pid, &status,0);
+		if(r == -1)
+			perror("wait"); //somehow an error during waiting
 		string init = arg[0];
-		if(contain_exit(arg));
-		else if(init == "true") return true;
-		else if(init == "false") return false;
-		return true;
+		if(contain_exit(arg));	//does it contain exit
+		else if(init == "true") return true;	//check if it was true
+		else if(init == "false") return false;	//check if it was false
+		if(status > 0)
+		{
+			//child ran execvp and it was an error
+			return false;
+		}
 	}
-	return false;
+	return true;
 }
 
 int main(int argc, char* argv[])
@@ -122,20 +125,20 @@ int main(int argc, char* argv[])
 		string semi_c = ";";
 		string num_s = "#";
 
-		getline(cin, user_input);
+		getline(cin, user_input);			//user input
 		int num_i = user_input.find(num_s, 0);
 		if(num_i >= 0)
 		{
-			user_input = user_input.substr(0,num_i);
+			user_input = user_input.substr(0,num_i);	//look for a comment
 		}
 		int index = 0;
-		string::iterator it;
-		for(it = user_input.begin(); it < user_input.end(); it++, index++)
+		string::iterator it;					//this is a helper function
+		for(it = user_input.begin(); it < user_input.end(); it++, index++) //love my variables
 		{
 			int wtf = user_input.find(semi_c, index);
 			int lamp = user_input.find(or_c, index);
 			int melon = user_input.find(and_c, index);
-			if(wtf >= 0 && (wtf < lamp || lamp == -1) && (wtf < melon || melon == -1))
+			if(wtf >= 0 && (wtf < lamp || lamp == -1) && (wtf < melon || melon == -1))//adds spaces if semi colon found
 			{
 				user_input.insert(user_input.find(semi_c, index), " ");
 
@@ -144,7 +147,7 @@ int main(int argc, char* argv[])
 				it += user_input.find(semi_c, index) +1;
 				index = user_input.find(semi_c, index) +1;
 			}
-			else if(lamp >= 0 && (lamp < wtf || wtf == -1) && (lamp < melon || melon == -1))
+			else if(lamp >= 0 && (lamp < wtf || wtf == -1) && (lamp < melon || melon == -1))//adds spaces if or connector found
 			{
 				user_input.insert(user_input.find(or_c, index), " ");
 			
@@ -153,7 +156,7 @@ int main(int argc, char* argv[])
 				it += user_input.find(or_c, index) + 2;
 				index = user_input.find(or_c, index) +2;
 			}
-			else if(melon >= 0 && (melon < wtf || wtf == -1) && (melon < lamp || lamp == -1))
+			else if(melon >= 0 && (melon < wtf || wtf == -1) && (melon < lamp || lamp == -1))//adds spaces if and connector found
 			{
 				user_input.insert(user_input.find(and_c, index), " ");
 			
@@ -165,84 +168,61 @@ int main(int argc, char* argv[])
 		}
 		char* womp = (char*)user_input.c_str();
 		char* token;
-		token = strtok(womp, " \t\n");
+		token = strtok(womp, " \t\n"); //token by spaces and tabs
 		while(token != NULL)
 		{
-			char** lol;
+			char** lol;		//love my variables
 			string condition;
-			bool prev_or = false;
+			bool prev_or = false; //conditions so the next value always run unless otherwise
 			bool prev_and = true;
 			while(token != NULL)
 			{
-				tokenizer(condition, lol, token);
+				tokenizer(condition, lol, token); //parses char** as commands, lol is the command, condition is the connector
 				if(*lol != NULL && !prev_or && prev_and)
-					if(contain_exit(lol)) exit(1);
+					if(contain_exit(lol)) exit(1);	//exits program
 				if(condition == or_c)
 				{
-					if(prev_or)
-					{
-						/*tokenizer(condition,lol,token);
-						if(*lol != NULL)
-							if(contain_exit(lol)) exit(1);*/
-					}
+					if(prev_or);			//if previous condition true, does not run exec
 					else if(!prev_and)
 					{
-						prev_and = true;
+						prev_and = true;	//case where previous condition was AND and that it failed the previous condition
 						prev_or = false;
 					}
-					else if(run_execvp(lol) == true)
+					else if(run_execvp(lol) == true)	//current statement is true
 					{
 						if(token != NULL)
-						{
-							//tokenizer(condition,lol,token);
-							prev_or = true;
-						}
-						else
-						{}
+							prev_or = true;		//sets to know that the previous condition under OR is true
 					}
 				}
 				else if(condition == and_c)
 				{
-					if(!prev_and)
-					{
-						/*tokenizer(condition,lol,token);
-						if(*lol != NULL)
-							if(contain_exit(lol)) exit(1);*/
-					}
+					if(!prev_and);			//if previous condition was false; does not run exec
 					else if(prev_or)
 					{
-						//if(run_execvp(lol));
-						/*
-						if(*lol != NULL)
-							if(contain_exit(lol)) exit(1);*/
+						prev_and = true;
 						prev_or = false;
 					}
-					else if(run_execvp(lol) == false)
+					else if(run_execvp(lol) == false)	//current statement is false
 					{
 						if(token != NULL)
-						{
-							//tokenizer(condition,lol,token);
 							prev_and = false;
-						}
-						else
-						{}
 					}
 
 				}
-				else if(condition == semi_c && !prev_or)
+				else if(condition == semi_c)	
 				{
-					if(run_execvp(lol));
-					prev_or = false;
+					if(!prev_or && prev_and)
+					{
+						if(run_execvp(lol));	//runs if previous or was false and if previous and was true
+					}
+					prev_or = false;	//resets conditions for passing exec
 					prev_and = true;
 				}
 				else
-				{
 					if(run_execvp(lol));
-				}
-
+				delete lol;	//remember to delete char** at end of command to prevent memory leaks
 
 			}
-			
 			break;
 		}
 	}
